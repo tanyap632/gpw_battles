b82df0ed7855047195c91f0a42220a217ccd93ed
Tanya Poh <tanyap632@gmail.com>
2025-08-25 21:08:13 +0500

    new

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({
   plugins: [vue(), vueJsx(), vueDevTools()],
-  base: mode === 'tanyap632' ? '/gpw_battles/' : '/',
+  base: '/gpw_battles/',
   build: {
     outDir: 'dist', // папка для сборки
     assetsDir: 'assets'
